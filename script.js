0a7135d486a27d0af5bda99a08939e454ff33f84
DanieleSavoia <danielesavoia44@gmail.com>
2023-05-18 18:44:17 +0200

    final

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            randomMail:0,
+            arrMail: [],
         }
     },
     methods: {
-        getRandomMail(){
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then(serverResponse => this.randomMail = serverResponse.data.response);
-        }
+       
     },
     created() {
-       this.getRandomMail(); 
+        for ( i = 0; i<10 ;i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(serverResponse => this.arrMail.push(serverResponse.data.response)); 
+        }
+       
     },
 });
 app.mount('#app')
+
